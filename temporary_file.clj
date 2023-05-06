659da318818de139784616b65b81e3f4b6fc2aab
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-05-06 23:53:11 +0530

    Create temporary_file.clj

---
+++ temporary_file.clj
@@ -0,0 +1,6 @@
+(defn my_health[me & time](
+if(and (me) (< time 180)) ;; In Minutes
+  (print "Today tired and unhealthy sticked with some issues taking rest two days either IDE runner going to launch soon with app note update. Thanks guys ! Support Always")
+))
+
+;; X Stollen and Writers 
