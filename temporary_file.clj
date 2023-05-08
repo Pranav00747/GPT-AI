5473d5dddf2eacf89f5c60c4a16380ff7bdad88d
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-05-08 21:30:31 +0530

    Update temporary_file.clj

--- temporary_file.clj
+++ temporary_file.clj
@@ -1,6 +1,11 @@
 (defn my_health[me & time](
 if(and (me) (< time 180)) ;; In Minutes
-  (print "Today tired and unhealthy sticked with some issues taking rest two days either IDE runner going to launch soon with app note update. Thanks guys ! Support Always")
+  if(= me "Seek Reported")
+  (print "Tired and unhealthy sticked with some issues taking rest some days extended either IDE runner going to launch soon with app note update. Thanks guys ! Support Always")
 ))
 
-;; X Stollen and Writers 
+(defn for_someone[people](
+   if(= people "Still stolen")
+     (print "It's an assignments not their own contribution not a product at all uploading university projects.")
+    ))
+;; X Stollen [Write Own Code] and Writers 
